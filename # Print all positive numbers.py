945be93da563c1ad366a02440c51ede99b57a57d
A_Hasan_C <legendofotherworlds@gmail.com>
2025-05-22 10:24:20 +0530

    # Print all positive numbers.py

--- # Print all positive numbers.py
+++ # Print all positive numbers.py
@@ -3,8 +3,18 @@ list1 = [12, -7, 5, 64, -14]
 for num in list1:
     if num > 0:
         print(num)
+        
+12
+5
+64
+
+
 
 list2 = [12, 14, -95, 3]
 for num in list2:
     if num > 0:
         print(num)
+        
+12
+14
+3
