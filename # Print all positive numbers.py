c78bbfbe32391765644cf93d0baca4a743fef9ad
A_Hasan_C <legendofotherworlds@gmail.com>
2025-05-22 10:22:59 +0530

    Add files via upload

---
+++ # Print all positive numbers.py
@@ -0,0 +1,10 @@
+# Print all positive numbers
+list1 = [12, -7, 5, 64, -14]
+for num in list1:
+    if num > 0:
+        print(num)
+
+list2 = [12, 14, -95, 3]
+for num in list2:
+    if num > 0:
+        print(num)
